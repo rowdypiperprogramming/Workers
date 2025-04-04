cba1e161025cb8389c4b53e61db8d547fb3f3202
rowdypiperprogramming <s2005493@online.houstonisd.org>
2025-04-04 13:11:42 -0500

    Create workers.sql

---
+++ workers.sql
@@ -0,0 +1,11 @@
+-- your code goes here
+CREATE TABLE workers (
+id INTEGER,
+age_as_of_2025 INTEGER,
+name VARCHAR(30)
+);
+INSERT INTO workers (id, age_as_of_2025, name)
+VALUES (1, 12, "Rowdy Piper");
+
+INSERT INTO workers (id, age_as_of_2025, name)
+VALUES (2, 12, "Jess McClesky");
